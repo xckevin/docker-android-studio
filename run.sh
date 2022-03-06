08d616eebf0253e4c5b99fe63f503f4d30e9c009
kevin.liu <xckevin927@gmail.com>
2022-03-06 11:58:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
+socat TCP-LISTEN:6000,reuseaddr,fork UNIX-
+export DISPLAY=:0
+export IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+echo $IP
+xhost +
+xhost +$IP
+
 docker run --rm  --name androidstudio -e DISPLAY=$IP:0 --net host -d  -v /tmp/.X11-unix:/tmp/.X11-unix xckevin927/android-studio
