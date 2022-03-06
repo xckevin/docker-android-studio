f36b15a4801b6a840335cb182d96b5b37d95de13
kevin.liu <xckevin927@gmail.com>
2022-03-06 12:00:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo $IP
 xhost +
 xhost +$IP
 
-docker run --rm  --name androidstudio -e DISPLAY=$IP:0 --net host -d  -v /tmp/.X11-unix:/tmp/.X11-unix xckevin927/android-studio
+docker run --rm  --name androidstudio -e DISPLAY=$IP:0 --net host -d  -v /tmp/.X11-unix:/tmp/.X11-unix android-dev
